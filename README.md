f8829d91563cd88340c33f801a8fb327d4806fb6
Pekka Nikander <pekka.nikander@iki.fi>
2020-05-17 16:45:53 +0300

    Add intial support for new format & better readme

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # creditfinvoice2csv
-OP Credit card Finvoice credit card bill to CSV
 
-Columns:
+Convert OP Credit card Finvoice invoice XML to CSV
+
+## Columns
 
         buyer: BuyerOrganisationName,
         seller: Parsed from ArticleIdentifier,
@@ -14,5 +15,42 @@ Columns:
         date: RowDeliveryDate,
         fileName: fileName
  
-Alternatives
-- PDF format invoices contain longer seller name, transaction number
+## Usage
+
+1. Install NodeJS to run this application from command line
+
+For Mac OS X:
+
+1.1. Install homebrew: https://brew.sh
+
+1.2. Use Homebrew to install NodeJS `brew install node`
+
+1.3. Test that it worked: `node --version`
+
+2. Use NodeJS Package Manager to install dependencies
+
+```sh
+npm install
+```
+
+3. Download your credit card invoice(s) as XML
+
+At the https://op.fi web site, do the following.  Currently OP serves only in Finnish or Swedish.
+
+3.1. Go to your e-invoice archive:  `Näytä lisää...` -> `E-laskuarkisto`
+
+3.2. Click the e-invoice you want to download
+
+3.3. Click `Katso laskun tiedot` to see the invoice information
+
+3.4. Click `Laskun tallennus omalle koneelle` to open options for storing your computer.
+
+3.5. Click `Tallenna lasku omalle koneelle` to download the invoice
+
+4. Run the application with `node`
+
+The app takes the input file names on command line and produces CSV to the standard output:
+
+```sh
+node index.js <xml-files> > output`
+```
--- index.js
+++ index.js
@@ -2,18 +2,27 @@
 
 var Promise = require('bluebird')
 var join = Promise.join
+var resolve = Promise.resolve
 var fs = Promise.promisifyAll(require('fs'))
 var xml2js = Promise.promisifyAll(require('xml2js'))
 var iconv = require('iconv-lite')
 var papaparse = require('papaparse')
-const BASE_PATH = './secret/'
 const ARTICLE_ID_SPLITTER = new RegExp(/^([^ ]+) ([^ ]+) ([^ ]+) (.+)$/)
+const SPECIFICATION_FREETEXT_SPLITTER = RegExp(/^([0-9]+.[0-9]+.) ([^ ]+) (.+) ([^ ]+) +([0-9]+,[0-9][0-9])$/)
 var _ = require('lodash')
 var moment = require('moment')
+const ignoredRows =
+  [ 'Kokonaissaldo',
+    'Suoritus',
+    'Viivästyskorko',
+    'Tilinhoitomaksu',
+    'ViivÃ€styskorko',
+    'Saldon siirto'
+  ]
 
-fs.readdirAsync(BASE_PATH)
+resolve(process.argv.slice(2))
   .map(function (fileName) {
-    var filePath = BASE_PATH + fileName
+    var filePath = fileName
     var stat = fs.statAsync(filePath)
     var contents = fs.readFileAsync(filePath).catch(function ignore () { })
       .then((contentBuffer) => {
@@ -30,26 +39,51 @@ fs.readdirAsync(BASE_PATH)
   })
   .filter((file) => file.contents !== undefined)
   .map(function (file) {
-    let invoiceTimeStamp = file.contents.Finvoice.MessageTransmissionDetails[0].MessageDetails[0].MessageTimeStamp[0]
-    return file.contents.Finvoice.InvoiceRow
-      // skips items that are not for bought items
-      .filter((row) => ['Kokonaissaldo', 'Suoritus', 'Viivästyskorko', 'Tilinhoitomaksu', 'ViivÃ€styskorko'].indexOf(row.ArticleIdentifier[0]) === -1)
-      .map((row) => {
-        let ArticleIdentifierMatch = row.ArticleIdentifier[0].match(ARTICLE_ID_SPLITTER)
-        if (!ArticleIdentifierMatch) throw new Error(`'Unknown format in ArticleIdentifier ${row.ArticleIdentifier[0]}`)
-        return {
-          buyer: file.contents.Finvoice.BuyerPartyDetails[0].BuyerOrganisationName[0],
-          seller: ArticleIdentifierMatch[4].trim(),
-          city: ArticleIdentifierMatch[3],
-          country: ArticleIdentifierMatch[2],
-          type: ArticleIdentifierMatch[1],
-          amount: parseFloat(row.RowAmount[0]._.replace(',', '.')),
-          currency: row.RowAmount[0].$.AmountCurrencyIdentifier.trim(),
-          card: row.ArticleName[0],
-          date: moment(row.RowDeliveryDate ? row.RowDeliveryDate[0]._ : invoiceTimeStamp),
-          fileName: file.fileName
-        }
-      })
+    let invoiceTimeStamp = file.contents.Finvoice.InvoiceDetails[0].InvoiceDate[0]._
+    let row = file.contents.Finvoice.InvoiceRow
+    if (row[0].ArticleIdentifier !== undefined) {
+      return row
+        // skips items that are not for bought items
+        .filter((row) => ignoredRows.indexOf(row.ArticleIdentifier[0]) === -1)
+        .map((row) => {
+          let ArticleIdentifierMatch = row.ArticleIdentifier[0].match(ARTICLE_ID_SPLITTER)
+          if (!ArticleIdentifierMatch) throw new Error(`'Unknown format in ArticleIdentifier ${row.ArticleIdentifier[0]}`)
+          return {
+            buyer: file.contents.Finvoice.BuyerPartyDetails[0].BuyerOrganisationName[0],
+            seller: ArticleIdentifierMatch[4].trim(),
+            city: ArticleIdentifierMatch[3],
+            country: ArticleIdentifierMatch[2],
+            type: ArticleIdentifierMatch[1],
+            amount: parseFloat(row.RowAmount[0]._.replace(',', '.')),
+            currency: row.RowAmount[0].$.AmountCurrencyIdentifier.trim(),
+            card: row.ArticleName[0],
+            date: moment(row.RowDeliveryDate ? row.RowDeliveryDate[0]._ : invoiceTimeStamp),
+            fileName: file.fileName
+          }
+        })
+    } else {
+      let details = file.contents.Finvoice.SpecificationDetails[0]
+      if (details !== undefined && details.SpecificationFreeText !== undefined) {
+        return details.SpecificationFreeText
+          .map((freetext) => {
+            let freeTextFields = freetext.match(SPECIFICATION_FREETEXT_SPLITTER)
+            if (freeTextFields == null) { return null }
+            return {
+              buyer: file.contents.Finvoice.BuyerPartyDetails[0].BuyerOrganisationName[0],
+              seller: freeTextFields[3].trim(),
+              city: freeTextFields[4],
+              country: '',
+              type: freeTextFields[2],
+              amount: parseFloat(freeTextFields[5].replace(',', '.')),
+              currency: 'EUR',
+              card: '',
+              date: freeTextFields[1] ? moment(freeTextFields[1], 'DD.MM.') : moment(invoiceTimeStamp),
+              fileName: file.fileName
+            }
+          })
+          .filter((row) => (row != null))
+      }
+    }
   })
   .then((data) => {
     data = _.flatten(data)
